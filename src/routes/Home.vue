56d3cf9b2158c4c1c8aa722fe9efc829ea152d68
mi-hee-k <kim323mh@gmail.com>
2023-02-16 09:37:20 +0900

    vuex 적용

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <Headline />
   <Search />
+  <MovieList />
 </template>
 
 <script>
 import Headline from '../components/Headline.vue';
 import Search from '../components/Search.vue';
+import MovieList from '../components/MovieList.vue';
 export default {
   components: {
     Headline,
     Search,
+    MovieList,
   },
 };
 </script>
