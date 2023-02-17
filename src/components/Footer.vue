93102e2f4691b0e0454087960e6ba35ad8312906
mi-hee-k <kim323mh@gmail.com>
2023-02-17 16:56:05 +0900

    refactoring & UI 추가

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <Logo />
+    <a href="https://github.com/mi-hee-k" target="_blank">
+      (C) {{ new Date().getFullYear() }} MIHEE
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '../components/Logo.vue';
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: 0.3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
--- src/store/movie.js
+++ src/store/movie.js
@@ -7,8 +7,9 @@ export default {
   // data
   state: () => ({
     movies: [],
-    message: '',
+    message: 'Search for the movie title!',
     loading: false,
+    theMovie: {},
   }),
   // computed
   getters: {},
@@ -26,33 +27,96 @@ export default {
   },
   // 비동기
   actions: {
+    // 영화 정보 요청
     async searchMovies({ state, commit }, payload) {
-      const { title, type, year, number } = payload;
-      const OMDB_API_KEY = '7035c60c';
-      const res = await axios.get(
-        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
-      );
-      const { Search, totalResults } = res.data;
+      if (state.loading) return;
       commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID'),
+        message: '',
+        loading: true,
       });
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1,
+        });
+        const { Search, totalResults } = res.data;
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID'),
+        });
 
-      const total = parseInt(totalResults, 10);
-      const pageLength = Math.ceil(total / 10);
+        const total = parseInt(totalResults, 10);
+        const pageLength = Math.ceil(total / 10);
 
-      // 추가 요청
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page += 1) {
-          if (page > number / 10) break;
-          const res = await axios.get(
-            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-          );
-          const { Search } = res.data;
-          commit('updateState', {
-            movies: [...state.movies, ..._uniqBy(Search, 'imdbID')],
-          });
+        // 추가 요청
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > payload.number / 10) break;
+            const res = await _fetchMovie({
+              ...payload,
+              page,
+            });
+            const { Search } = res.data;
+            commit('updateState', {
+              movies: [...state.movies, ..._uniqBy(Search, 'imdbID')],
+            });
+          }
         }
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message,
+        });
+      } finally {
+        commit('updateState', {
+          loading: false,
+        });
+      }
+    },
+    // 단일 영화 정보 요청
+    async searchMovieWithId({ state, commit }, payload) {
+      if (state.loading) return;
+      commit('updateState', {
+        theMovie: {},
+        loading: true,
+      });
+      try {
+        const res = await _fetchMovie(payload);
+        commit('updateState', {
+          theMovie: res.data,
+        });
+      } catch (error) {
+        commit('updateState', {
+          theMovie: {},
+        });
+      } finally {
+        commit('updateState', {
+          loading: false,
+        });
       }
     },
   },
 };
+
+// 데이터 요청 로직
+function _fetchMovie(payload) {
+  const { title, type, year, page, id } = payload;
+  const OMDB_API_KEY = '7035c60c';
+  const url = id
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        // 예외처리 - imDb에서 잘못된 요청일 경우에도 정상으로 응답하는 경우가 있음
+        if (res.data.Error) {
+          reject(res.data.Error);
+        }
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+}
