225c89dc62bb777c4f6a22ca8970cd51d8dafad7
mi-hee-k <kim323mh@gmail.com>
2023-02-20 11:49:36 +0900

    About 페이지

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'MiHee',
+    email: 'ukoko@naver.com',
+    blog: 'https://firstlife-note.tistory.com/',
+    phone: '+82-10-123-4567',
+    image: 'https://heropy.blog/css/images/logo.png',
+  }),
+};
